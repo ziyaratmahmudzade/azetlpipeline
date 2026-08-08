7ffcbb3c76439e74e4f9bc8b162c832b63ddb93b
ziyaratmahmudzade <150405732+ziyaratmahmudzade@users.noreply.github.com>
2026-08-08 16:01:27 +0200

    Create nb_mount_adls.py

---
+++ notebooks/nb_mount_adls.py
@@ -0,0 +1,24 @@
+storage_account = "saetlpipeline100"
+container       = "datalake100"        
+
+spark.conf.set(
+    f"fs.azure.account.key.{storage_account}.dfs.core.windows.net",
+    dbutils.secrets.get(scope="kv-scope", key="adls-storage-key")
+)
+
+# List everything inside the container
+base = dbutils.fs.ls(f"abfss://{container}@{storage_account}.dfs.core.windows.net/")
+for b in base:
+    print(b.path+"\n")
+
+# Check raw/sales/
+print("=== raw/sales/ ===")
+sales_files = dbutils.fs.ls(f"abfss://datalake100@saetlpipeline100.dfs.core.windows.net/raw/sales/")
+for f in sales_files:
+    print(f.path)
+
+# Check raw/master/
+print("\n=== raw/master/ ===")
+master_files = dbutils.fs.ls(f"abfss://datalake100@saetlpipeline100.dfs.core.windows.net/raw/master/")
+for f in master_files:
+    print(f.path)
