13344571f0336c999402051e1c297023ae7ed8d9
ziyaratmahmudzade <mahmudzadaziyaratl@gmail.com>
2026-08-11 17:25:29 +0200

    Remove .venv directory from Git tracking

--- .venv/Lib/site-packages/msal/sku.py
+++
@@ -1,6 +0,0 @@
-"""This module is from where we receive the client sku name and version.
-"""
-
-# The __init__.py will import this. Not the other way around.
-__version__ = "1.37.0"
-SKU = "MSAL.Python"
